9379be966d7aae156ef4e57b0387c2f1be359698
Omri <paffon@gmail.com>
2021-04-20 22:33:48 +0200

    Added A* search

---
+++ Maze/src/searches/AStarSearch.java
@@ -0,0 +1,65 @@
+package searches;
+
+import grid.Grid;
+import grid.Point;
+import grid.Square;
+import grid.SquareType;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class AStarSearch extends Search {
+    PriorityQueue<Square> pq;
+
+    public AStarSearch(Grid grid, Square agentSquare, Square goalSquare) {
+        super(grid, agentSquare, goalSquare);
+
+        pq = new PriorityQueue<Square>(rows * cols,
+                Comparator.comparing(x -> x.distance + heuristic(x)));
+        pq.add(agent);
+    }
+
+    public double heuristic(Square square) {
+        return manhattanDistance(square, goal);
+    }
+
+    private double manhattanDistance(Square s1, Square s2) {
+        double deltaX = Math.abs(s1.getGridX() - s2.getGridX());
+        double deltaY = Math.abs(s1.getGridY() - s2.getGridY());
+        return deltaX + deltaY;
+    }
+
+    @Override
+    public void next() {
+        if(!pq.isEmpty()) { // still something left to explore
+            Square currentSquare = pq.poll();
+            if(currentSquare.equals(goal)) {
+                currentSquare.setType(SquareType.GOAL_FOUND);
+                System.out.println("Found a path");
+                System.out.println("distance = " + currentSquare.distance);
+                pathFound = true;
+                return;
+            }
+
+            if(! currentSquare.isAgent()) {
+                currentSquare.setType(SquareType.VISITED);
+            }
+
+            visited.add(currentSquare);
+
+            Set<Square> neighbours = grid.getNeighbours(currentSquare);
+            for(Square neighbour : neighbours) {
+                if(! (neighbour.isWall() || visited.contains(neighbour) || pq.contains(neighbour) ) ) {
+                    neighbour.distance = currentSquare.distance + 1;
+                    neighbour.setType(SquareType.TO_VISIT);
+                    pq.add(neighbour);
+                }
+            }
+        }
+        else {
+            System.out.println("Path DOESN'T EXIST");
+            pathMightExist = false;
+        }
+    }
+}
